8f5e3be04f5326be76d087d0b7bdeaf8c9109455
Kaan Bora Öz <kaanboraoz@proton.me>
2026-04-20 21:09:10 +0300

    chore: explore image crate and implement basic image resizing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use std::{fs, path::{Path, PathBuf}};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use clap::Parser;
+use image::ImageReader;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    format: String,
+    path: String,
 
     #[arg(short, long)]
     name: String,
@@ -15,20 +19,24 @@ struct Args {
     size: i32,
 
     #[arg(short, long)]
-    path: PathBuf,
+    output: String,
 
-    #[arg(short, long)]
+    #[arg(short, long, alias = "loc")]
     locate: PathBuf,
 }
 
 fn main() -> Result<(), std::io::Error> {
-    // let args: Args = Args::parse();
+    let args: Args = Args::parse();
 
-    let my_dir = fs::read_dir(&Path::new("./images"))?
-    .map(|res| res.map(|e| e.path()))
-    .collect::<Result<Vec<_>, std::io::Error>>()?;
+    let my_dir = fs::read_dir(&Path::new(&args.path))?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, std::io::Error>>()?;
 
-    println!("{:?}", my_dir);
+    let img = ImageReader::open(my_dir[0].as_path())?.decode().unwrap();
+
+    let nimg = img.resize(200, 200, image::imageops::FilterType::Nearest);
+
+    let _ = nimg.save("./images/new.png");
 
     Ok(())
 }
